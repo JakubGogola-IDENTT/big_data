3accfcbcc131fbaa355ca822cfa8dcf512c8b5af
JakubGogola <jakubgogola97@gmail.com>
2020-10-13 17:10:33 +0200

    improvements + clouds generated

--- words_cloud/index.js
+++ words_cloud/index.js
@@ -3,8 +3,8 @@ import fs from 'fs';
 const whitespacesRegEx = /[\s\d]+/g;
 const charsRegEx = /[\[\]\.,:;<>()#'"-_`~$?!]/g
 
-const stopwordsFile = fs.readFileSync('./stopwords_pl.txt', { encoding: 'utf-8' });
-const textFile = fs.readFileSync('./pt_pl.txt', { encoding: 'utf-8' });
+const stopwordsFile = fs.readFileSync('./stopwords_en.txt', { encoding: 'utf-8' });
+const textFile = fs.readFileSync('./bc_en.txt', { encoding: 'utf-8' });
 
 const stopwords = stopwordsFile
         .replace(whitespacesRegEx, ' ')
@@ -31,7 +31,7 @@ const sortedWords = Object.entries(wordsCount)
         .sort(([, count1], [, count2]) => count2 - count1);
 
 const fileContent = sortedWords
-        .slice(0, 50)
+        .slice(0, 200)
         .reduce((acc, [word, count]) => acc.concat(`${count};${word};\n`), '');
 
 fs.writeFileSync('result.csv', fileContent);
